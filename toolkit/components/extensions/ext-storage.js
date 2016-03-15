449fcf067df2af836fbc853551612f9fbebdb30c
Michiel de Jong <mbdejong@mozilla.com>
2016-03-15 10:35:22 +0100

    Bug 1253744 - Implement extension.storage.sync.enabled config flag

--- toolkit/components/extensions/ext-storage.js
+++ toolkit/components/extensions/ext-storage.js
@@ -1,14 +1,29 @@
 "use strict";
 
+const STORAGE_SYNC_ENABLED = 'extension.storage.sync.enabled';
+
 var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
 XPCOMUtils.defineLazyModuleGetter(this, "ExtensionStorage",
                                   "resource://gre/modules/ExtensionStorage.jsm");
+XPCOMUtils.defineLazyModuleGetter(this, "Preferences",
+                                  "resource://gre/modules/Preferences.jsm");
 
 Cu.import("resource://gre/modules/ExtensionUtils.jsm");
 var {
   EventManager,
 } = ExtensionUtils;
+
+function rejection(message) {
+  return Promise.reject({ message });
+}
+
+function checkEnabled() {
+  if (Preferences.get(STORAGE_SYNC_ENABLED, false) !== true) {
+    return rejection(`Please set ${STORAGE_SYNC_ENABLED} to true in about:config`);
+  }
+  return Promise.resolve();
+}
 
 extensions.registerSchemaAPI("storage", "storage", (extension, context) => {
   return {
@@ -29,6 +44,29 @@ extensions.registerSchemaAPI("storage", "storage", (extension, context) => {
         clear: function(callback) {
           return context.wrapPromise(
             ExtensionStorage.clear(extension.id), callback);
+        },
+      },
+
+      sync: {
+        get: function(keys, callback) {
+          return context.wrapPromise(checkEnabled().then(() => {
+            return rejection('Not implemented');
+          }), callback);
+        },
+        set: function(items, callback) {
+          return context.wrapPromise(checkEnabled().then(() => {
+            return rejection('Not implemented');
+          }), callback);
+        },
+        remove: function(items, callback) {
+          return context.wrapPromise(checkEnabled().then(() => {
+            return rejection('Not implemented');
+          }), callback);
+        },
+        clear: function(callback) {
+          return context.wrapPromise(checkEnabled().then(() => {
+            return rejection('Not implemented');
+          }), callback);
         },
       },
 
